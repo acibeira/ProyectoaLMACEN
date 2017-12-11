ab2feb03f155f2104b4ccc74c376c9dc551713ed
acibeira <adridc7@gmail.com>
2017-12-11 20:25:37 +0100

    Estado del proyecto inicial del proyecto

---
+++ index.js
@@ -0,0 +1,146 @@
+/**
+ * @file Aplicación para la gestión de un almacen
+ * @author Adrian
+ * @version 0.1
+ */
+
+/**
+  * @class Almacen respresenta almacen de CDs y DVDs
+  * @property {Array} cds Array de objetos tipo CD
+  * @prop {Array} dvds Array de objetos tipo DVD
+  * @property {number} capacidad capacidad del almacén
+  */
+
+class Almacen {
+    //Constructor de la clase almacén, recibe la cantidad de elementos (cds o dvds) que puede haber. 
+    /**
+     * Crea un almacen
+     * @param {numbre} capacidad capacidad del almacén
+     */
+    constructor(capacidad) {
+        this.capacidad = capacidad;
+        this.cds = [];
+        this.dvds = [];
+    }
+    
+
+    //El método introducir recibe un objeto y si hay sitio, tras comprobar si es de tipo cd o dvd lo mete dentro de la colección (Array) correspondiente. Devuelve 0 si no puede meterlo o en caso contrario el número de unidades introducidas.
+
+    /**
+     * 
+     * @param {*} objeto objeto que introducimos en el almacén 
+     * @returns {numbre} Valor devuelto: 0 almacen lleno; sino número de unidades introducidas
+     */ 
+    introducir(objeto) {
+        if (this.capacidad == 0) {
+            console.log("almacen lleno")
+            return 0;
+        }
+        if (objeto.unidades <= this.capacidad) {
+            if (objeto.tipo == "cd") {
+                this.cds.push(objeto)
+            } else if (objeto.tipo == "dvd") {
+                this.dvds.push(objeto)
+            } else {
+                return 0;
+            }
+            this.capacidad = this.capacidad - objeto.unidades;
+            return objeto.unidades
+        } else {
+            if (objeto.tipo == "cd") {
+                objeto.unidades = this.capacidad;
+                this.capacidad = 0
+                this.cds.push(objeto)
+            } else if (objeto.tipo == "dvd") {
+                objeto.unidades = this.capacidad;
+                this.capacidad = 0
+                this.dvds.push(objeto)
+            } else {
+                return 0;
+            }
+            return objeto.unidades
+        }
+    }
+    //Este método recibe una cadena de texto y busca dentro del almacén si existe un cd o dvd con ese título o ese autor, si lo encuentra devulve un objeto con el título, autor, unidades y precio. Si no lo encuentra devuelve null
+
+
+    /**
+     * 
+     * @param {string} tituloautor objeto que buscamos en la funcion
+     * @returns {string} si lo encuentra devuelve un objeto si no devuelve null
+     */
+    buscar(tituloautor) {
+        for (let obj of this.cds) {
+            if ((tituloautor == obj.titulo) || (tituloautor == obj.autor)) {
+                let objDevuelto = {
+                    titulo: obj.titulo,
+                    autor: obj.autor,
+                    unidades: obj.unidades,
+                    precio: obj.precio
+                };
+                return objDevuelto;
+            }
+        }
+        for (let obj of this.dvds) {
+            if ((tituloautor == obj.titulo) || (tituloautor == obj.autor)) {
+                let objDevuelto = {
+                    titulo: obj.titulo,
+                    autor: obj.autor,
+                    unidades: obj.unidades,
+                    precio: obj.precio
+                };
+                return objDevuelto;
+            }
+        }
+        return null;
+    }
+    //El método comprar, recibe una cadena de texto con un título y la cantidad de dinero. Devuelve -1 si no puede comprarlo y la cantidad de dinero que le sobra si lo puede comprar.
+    /**
+     * 
+     * @param {*} titulo objeto que buscamos
+     * @param {*} dinero cantidad que disponemos
+     * @returns {number} devuelve -1 si no se puede comprar  o la cantidad de dinero que sobra si se puede
+     */
+    comprar(titulo, dinero) {
+        for (let obj of this.cds) {
+            if (titulo == obj.titulo) {
+                if (dinero >= obj.precio) {
+                    this.capacidad++;
+                    obj.unidades--;
+                    return dinero - obj.precio;
+                } else {
+                    return -1;
+                }
+            }
+        }
+        for (let obj of this.dvds) {
+            if (titulo == obj.titulo) {
+                if (dinero >= obj.precio) {
+                    this.capacidad++;
+                    obj.unidades--;
+                    return dinero - obj.precio;
+                } else {
+                    return -1;
+                }
+            }
+        }
+        return -1;
+    }
+
+   
+}
+
+/**************Empieza el Programa******************** */
+let fs=require("fs");
+let texto=fs.readFileSync("catalogo.json","utf-8");
+let catalogo=JSON.parse(texto);
+
+
+let alm1 = new Almacen(150);
+for(let elemento of catalogo){
+    console.log(alm1.introducir(elemento))
+}
+
+
+
+
